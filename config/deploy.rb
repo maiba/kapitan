d78b0f8c681a1e90b38bb1062ec89bc0a9789b5b
gray <graycode.studio@gmail.com>
2011-06-21 12:29:28 +0300

    Fix service offers left margin

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,54 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# _your_login_ - Поменять на ваш логин в панели управления
+# _your_project_ - Поменять на имя вашего проекта
+# _your_server_ - Поменять на имя вашего сервера (Указано на вкладке "FTP и SSH" вашей панели управления)
+# set :repository - Установить расположение вашего репозитория
+# У вас должна быть настроена авторизация ssh по сертификатам
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "kapitan"
+set :repository,  "git://github.com/graycoder/kapitan.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+dpath = "/home/hosting_tolik925/projects/kapitan"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :user, "hosting_tolik925"
+set :use_sudo, false
+set :deploy_to, dpath
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :scm, :git
+
+role :web, "lithium.locum.ru"                          # Your HTTP server, Apache/etc
+role :app, "lithium.locum.ru"                          # This may be the same as your `Web` server
+role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
+
+after "deploy:update_code", :copy_database_config
+after "deploy:update_code", :install_gems
+
+task :copy_database_config, roles => :app do
+  db_config = "#{shared_path}/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
+
+task :install_gems, roles => :app do
+  run "cd #{release_path} && /var/lib/gems/1.8/bin/bundle install --path=~/.gems"
+end
+
+set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
+set :unicorn_conf, "/etc/unicorn/kapitan.tolik925.rb"
+set :unicorn_pid, "/var/run/unicorn/kapitan.tolik925.pid"
+
+# - for unicorn - #
+namespace :deploy do
+  desc "Start application"
+  task :start, :roles => :app do
+    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+end
