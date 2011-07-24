374e98ad8b91cca22dbb66e8d5e49b800149b1fe
Sallyca <catherine.vladimirskaya@gmail.com>
2011-07-24 13:14:16 +0300

    Update crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,6 @@
 set :output, {:standard => '/log/cron.log'}
-job_type :rake_bundle,    "cd /var/lib/gems/1.8/bin && RAILS_ENV=:environment bundle exec rake :task :output"
 
 every 1.day do
-   rake_bundle "parse:news"
+   rake "parse:news"
 end
 
