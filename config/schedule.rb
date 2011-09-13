453983b78084c3239e04a45fefb20a55ee59bb69
Sallyca <catherine.vladimirskaya@gmail.com>
2011-09-13 15:52:46 +0300

    Crontab whenever task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,4 @@
-set :output, '/log/whenever.log'
-job_type :rake_bundle,    "cd /var/lib/gems/1.8/bin/bundle exec rake :task :output"
+job_type :rake_bundle,    "/var/lib/gems/1.8/bin/bundle exec rake :task"
 
 every 1.day do
   rake_bundle "parse:news"
