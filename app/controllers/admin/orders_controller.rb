2e2f18433fc125210e3b9681eef85aff4c972f4e
Sallyca <catherine.vladimirskaya@gmail.com>
2011-12-12 09:58:11 +0200

    Sorting orders in reFinery

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,22 @@
 module Admin
   class OrdersController < Admin::BaseController
 
+    helper_method :sort_column, :sort_direction
+
     crudify :order, :xhr_paging => true
+
+    def index
+      @orders = Order.order(sort_column + ' ' + sort_direction).paginate(:page => params[:page])
+    end
+
+    private
+     def sort_column
+       Order.column_names.include?(params[:sort]) ? params[:sort] : "status"
+     end
+
+     def sort_direction
+       %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+     end
 
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     sprintf("%.2f", price)
   end
 
+ def sortable(column, title = nil)
+    title ||= Order.human_attribute_name(column)
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_direction && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
 end
