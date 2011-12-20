d0f9ca05301c9a39e4ba3b71ddd7139227171db7
Riazanov Dmytro <ryaz@km.ru>
2011-12-20 20:38:34 +0200

    Filenames in up and download parameterized

--- app/models/account/attachment.rb
+++ app/models/account/attachment.rb
@@ -3,9 +3,18 @@ class Account::Attachment < ActiveRecord::Base
   belongs_to :account, :class_name => "::Account", :foreign_key => "account_id"
 
   has_attached_file :resource
+  before_resource_post_process :transliterate_file_name
 
   validates :account, :presence => true
   validates_attachment_presence :resource
   validates_attachment_size :resource, :less_than => 2.megabytes
+  
+  private
+
+  def transliterate_file_name
+    extension = File.extname(resource_file_name).gsub(/^\.+/, '')
+    filename = resource_file_name.gsub(/\.#{extension}$/, '')
+    self.resource.instance_write(:file_name, "#{filename.parameterize}.#{extension.parameterize}")
+  end
 
 end
