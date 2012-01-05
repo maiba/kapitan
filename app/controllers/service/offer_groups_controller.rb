e87e80a15c861ea456d58ac7cb42b804fdf99dcd
Sallyca <catherine.vladimirskaya@gmail.com>
2012-01-05 12:50:21 +0300

    JS offer groups on users view (+discounts)

--- app/controllers/service/offer_groups_controller.rb
+++ app/controllers/service/offer_groups_controller.rb
@@ -1,11 +1,13 @@
 class Service::OfferGroupsController < ServicesController
 
   prepend_before_filter :find_offer_group, :only => [:show]
-  
+
   def properties
-    render :json => Offer::Type.find(params[:id]).offer_properties.to_json(:only => [ :id, :title ], :include => { :offer_property_options => { :only => [:id, :title, :price] } })
+    render :json => Offer::Type.find(params[:id]).offer_properties.to_json(:only => [ :id, :title ],
+     :include => { :offer_property_options => { :only => [:id, :title, :price]},
+                    :offer_type_property_discounts => { :only => [:id, :limit, :percent] }})
   end
-  
+
   def type
     render :json => Offer::Type.find(params[:id]).to_json(:only => [ :description, :pic ])
   end
