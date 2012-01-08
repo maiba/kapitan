f0d1f2d4c3cd144f6cc42686a9ee0d543520cc10
Sallyca <catherine.vladimirskaya@gmail.com>
2012-01-08 12:06:29 +0300

    Cabinet sorting

--- app/controllers/cabinet/orders_controller.rb
+++ app/controllers/cabinet/orders_controller.rb
@@ -1,11 +1,22 @@
 class Cabinet::OrdersController < CabinetController
+  
+  helper_method :sort_column, :sort_direction
 
   def index
-    @orders = current_account.orders
+    @orders = current_account.orders.order(sort_column + ' ' + sort_direction)
   end
 
   def show
     @order = current_account.orders.find(params[:id])
   end
+
+   private
+     def sort_column
+       Order.column_names.include?(params[:sort]) ? params[:sort] : "status"
+     end
+
+     def sort_direction
+       %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+     end
 
 end
